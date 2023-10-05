1481f187c4bba81bdf337786165d3be94210fa8d
Saugat Lamichhane <lamichhanesaugat62@gmail.com>
2023-10-05 19:06:58 +0545

    learned about array

--- src/Array.kt
+++ src/Array.kt
@@ -1,2 +1,12 @@
-package src
+fun main() {
+    //although not recommended, array can be heterogeneous
+    val myArray = arrayOf("Saugat", "Sushank", 3)
 
+    //prints memory address
+    println(myArray)
+
+    //prints members of array
+    println(myArray[0])
+    println(myArray[1])
+    println(myArray[2])
+}
