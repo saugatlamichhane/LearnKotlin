cd9871c946f8f679ffc9e8d4c43af7289171ab24
Saugat Lamichhane <lamichhanesaugat62@gmail.com>
2023-10-05 20:07:47 +0545

    learned about list

--- src/List.kt
+++ src/List.kt
@@ -1,2 +1,24 @@
-package src
+fun main(){
+    val myList = mutableListOf("Saugat", "Sushank", "Sudip")
+    //unlike arrays, the size of list can be changed
+    //remove() for removing a member of an array
+    myList.remove("Saugat")
+    //removeAt() for removing the member at certain index
+    myList.removeAt(1)
+    //add() for adding a member to the list
+    myList.add("Trailblazer")
+    //printing members of list
+    for(item in myList)
+        println(item)
 
+    //taking user input and storing in list
+    val usrList = mutableListOf<String>()
+    for(i in 0 until 6) {
+        val usrInput = readLine()
+        if(usrInput != null) {
+            usrList.add(usrInput)
+        }
+    }
+    //printing list at once
+    println(usrList)
+}
