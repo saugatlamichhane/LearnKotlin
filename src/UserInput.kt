49a5ae4cf2fd772bacd08a4fe43e8159e4bd8572
Saugat Lamichhane <lamichhanesaugat62@gmail.com>
2023-10-05 18:48:06 +0545

    added another way of null-checking and, as TODO, some topics to learn

--- src/UserInput.kt
+++ src/UserInput.kt
@@ -5,7 +5,16 @@ fun main() {
     //converting to int
     //TODO check if it is Int type
     val usrInput2 = readLine()
+    //check if user-input is null
     if (usrInput2 != null) {
         println(usrInput2.toInt() + 8)
     }
+
+    //alternative way: check if user-inputted int is null
+    val usrInput3 = readLine()?.toInt()
+    if(usrInput3 != null) {
+        println(usrInput3)
+    }
+
+    //TODO learn about readln() and readlnOrNull()
 }
