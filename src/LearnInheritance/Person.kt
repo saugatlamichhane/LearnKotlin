e03adc97353007e9079ad11a213f2031d864b904
Saugat Lamichhane <lamichhanesaugat62@gmail.com>
2023-10-09 22:43:10 +0545

    learned about visibility modifiers

--- src/LearnInheritance/Person.kt
+++ src/LearnInheritance/Person.kt
@@ -1,7 +1,11 @@
 package src.LearnInheritance
 
 open class Person(name: String, sex: String, var age: Int, var address: String) {
-
+    //visibility modifiers
+    //public: visible within any class
+    //private: visible within the same class only
+    //protected: visible within the same and inherited class
+    //constructor can also be private
     init {
         println("Hello, I am $name, a $age years old $sex, from $address")
     }
